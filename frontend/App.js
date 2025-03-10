ccd534d32f0ddf137640ed152868ec36bf245790
Sanjay Anasuri <sanasur@purdue.edu>
2025-03-10 19:29:37 -0400

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DJAssistant from "./components/DJAssistant";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DJ Assistant</h1>
+      <DJAssistant />
+    </div>
+  );
+}
+
+export default App;
